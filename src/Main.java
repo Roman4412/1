0f03a05cbf26c50fe115393e06568339ac66e596
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-02-14 02:19:33 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        String test = "Мир озарим  и разорим";
+        String formattedTest = test.replaceAll(" ", "");
+        System.out.println(reverse(formattedTest));
+        System.out.println(formattedTest);
+        System.out.println(formattedTest.equalsIgnoreCase(reverse(formattedTest)));
+
+
+        StringBuilder str = new StringBuilder(formattedTest);
+        str.reverse();
+        System.out.println(formattedTest.equalsIgnoreCase(str.toString()));
+
+    }
+
+    public static String reverse(String str) {
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0 ; i--) {
+            reverse += str.charAt(i);
+        }
+        return reverse;
+    }
+}
